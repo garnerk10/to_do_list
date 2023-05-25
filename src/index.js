7be24cec17ba264ea439ea5865d5e859e90da563
Kyle Garner <garnerk10@gmail.com>
2023-05-25 14:27:28 -0400

    basic layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-console.log('Pizza!')
+import { format } from 'date-fns';
+import './style.css';
+
+const createContent = (() => {
+    const content  = document.createElement('div');
+    content.setAttribute('id', 'content');
+    document.body.appendChild(content);
+})();
+const content = document.getElementById('content');
+
+
+//Create header bar
+const createHeader = (() => {
+    const header = document.createElement('div');
+    header.setAttribute('id', 'header');
+    header.setAttribute('class', 'header');
+    content.appendChild(header);
+
+        const title = document.createElement('h1');
+        title.innerText = "To-Do List";
+        header.appendChild(title);
+})();
+
+const sideBar = (() => {
+    const sideBar = document.createElement('div');
+    sideBar.setAttribute('id', 'sideBar');
+    sideBar.setAttribute('class', 'sideBar');
+    content.appendChild(sideBar);
+
+        const viewAll = document.createElement('h2');
+        viewAll.innerText = 'View All';
+        sideBar.appendChild(viewAll);
+
+        const projectsSideDiv = document.createElement('div');
+        projectsSideDiv.setAttribute('id', 'projectsSideDiv');
+        sideBar.appendChild(projectsSideDiv);
+
+            const projectsSideLabel = document.createElement('h3');
+            projectsSideLabel.innerText = "Projects";
+            projectsSideDiv.appendChild(projectsSideLabel);
+
+            const projectsSideHolder = document.createElement('div');
+            projectsSideHolder.setAttribute('id', 'projectsSideHolder');
+            projectsSideHolder.setAttribute('class', 'projectsSideHolder');
+            sideBar.appendChild(projectsSideHolder);
+})();
